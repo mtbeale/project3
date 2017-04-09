5401a75953faa6aaff4afecc77ed19813f93fb76
Michael Beale <mtbeale@hotmail.com>
2017-04-09 18:55:01 -0400

    Create env.cpp

---
+++ env.cpp
@@ -0,0 +1,9 @@
+#include <cstdlib>
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(const int argc, const char * argv[], const char * env[]) {
+  for (int i = 0; env[i] != NULL; i++)
+    printf("%s\n",env[i]);
+  return 0;
+}
