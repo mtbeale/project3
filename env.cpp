b55507887f89b54070abd1ee1602a8e9c93ac453
Michael Beale <mtbeale@hotmail.com>
2017-04-11 19:45:12 -0400

    done

--- env.cpp
+++ env.cpp
@@ -3,7 +3,9 @@
 #include <stdlib.h>
 
 int main(const int argc, const char * argv[], const char * env[]) {
+  
   for (int i = 0; env[i] != NULL; i++)
     printf("%s\n",env[i]);
+  
   return 0;
 }
