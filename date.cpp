6665c49026d4894e3b704204b087b64a6aee0dee
Michael Beale <mtbeale@hotmail.com>
2017-04-11 23:26:00 -0400

    done

--- date.cpp
+++ date.cpp
@@ -8,19 +8,30 @@
 #include <sys/stat.h>
 #include <sys/types.h>
 
-using namespace std;
 
+using namespace std;
 
 const char * get_timestamp() {
   time_t now = time (NULL);
-  return asctime (localtime (&now));
+
+  return asctime(localtime (&now));
 }
 
 int main(const int argc, const char * argv[]) {
+  char buffer [1024];
 
-  const char * timestamp = get_timestamp();
-  size_t length = strlen(timestamp);
+  if (argc == 2) {
+
+    time_t now = time(NULL);
+    struct tm * timeinfo;
+    time(&now);
+    timeinfo = localtime(&now);
+    strftime(buffer,80,argv[1],timeinfo);
+    puts(buffer);
+
+  } else {
+    const char * timestamp = get_timestamp();
 
   write(STDOUT_FILENO,timestamp ,50);
-
+  }
 }
