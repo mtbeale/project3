0e17003120cbffc5ee85a450d85c77b1b42fa1c3
Michael Beale <mtbeale@hotmail.com>
2017-04-11 20:51:27 -0400

    95% functional

--- wc.cpp
+++ wc.cpp
@@ -10,43 +10,67 @@
 
 using namespace std;
 
+
+
 int main(const int argc, char * argv[]) {
 
-  int lines=0, words=0, bytes = 0;
-  char word[30];
+  int lines=0, words=0,bytes = 0;; //counts
+  int tlines=0, twords=0, tbytes=0; //total counts
   string w, l;
   ifstream in;
-  struct stat filest;
+  struct stat filest; //for bytes
+  bool cflg = false, mflg = false, lflg=false, wflg=false;
 
   int o;
-  while ((o = getopt(argc, argv,"cmlw")) != -1)
+  while ((o = getopt(argc, argv,"cmlw")) != -1){
     switch (o) {
     case 'c':
+      cflg = true;
       break;
     case 'm':
+      mflg = true;
       break;
     case 'l':
+      lflg = true;
       break;
     case 'w':
+      wflg = true;
       break;
     }
+  }
 
   for (int i =optind; i < argc; i++) {
   in.open(argv[i]);
   stat(argv[i], &filest);
+
   while (in >> w){
     words++;
   }
-  in.close();
+   in.close();
   in.open(argv[i]);
   while (getline(in,l)) {
     lines++;
   }
+ bytes = filest.st_size;
 
-   cout << lines << " " << words << " "
-        << filest.st_size << " " <<  argv[i] << endl;
+  if (lflg)
+    cout << lines << " ";
+   if (wflg)
+    cout << words << " ";
+   if (cflg)
+     cout << filest.st_size << " ";
 
+   cout << argv[i] << endl;
+   twords+=words;
+   tlines+=lines;
+   tbytes+=bytes;
+   words = 0;
+   lines = 0;
+   bytes = 0;
    in.close();
+  }
+  if (argc > 1){
+    cout << tlines << " " << twords << " " << tbytes << " " << "total" << endl;
   }
    return 0;
 }
